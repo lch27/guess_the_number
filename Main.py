305fb4e18e02503d4de0f47d7239a8fd75bb4390
lch27 <mag.anderson96@gmail.com>
2024-01-01 18:25:10 +0900

    add: number guessing game script

--- Main.py
+++ Main.py
@@ -1,2 +1,13 @@
 import random
 
+secretNum = random.randint(1, 20)
+print('I am thinking about a number between 1 and 20.')
+
+for guessesTaken in range(1, 7):
+    print('Take a guess ^^')
+    guess = int(input())
+
+    if guess < secretNum:
+        print('Your guess is too low.')
+    elif guess > secretNum:
+        print('your guess is bigger than the number')
